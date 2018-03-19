181f40033956505df43b8e70b864a351ecc18eed
Lucas Dantas <lucas@fulllab.com.br>
2018-03-19 11:31:39 -0300

    specifying up and down into users migration

--- project/db/migrate/20180315130610_create_users.rb
+++ project/db/migrate/20180315130610_create_users.rb
@@ -1,9 +1,13 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
-  def change
-    create_table :users do |t|
-      t.string :name
+  def up
+    create_table(:users) do |t|
+      t.string(:name)
       t.string :email
       t.datetime :birth_date
     end
+  end
+
+  def down
+    drop_table(:users) if table_exists?(:users)
   end
 end
