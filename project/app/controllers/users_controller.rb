82f0cc17bf216a872390f3dc587e2d65783da76b
Lucas Dantas <lucas@fulllab.com.br>
2018-03-19 11:23:43 -0300

    more changes...

--- project/app/controllers/users_controller.rb
+++ project/app/controllers/users_controller.rb
@@ -1,2 +1,43 @@
 class UsersController < ApplicationController
+
+  # CRUD
+  # Create
+  # Read
+  # Update
+  # Destroy
+
+  # GET: /users
+  def index
+    @users = User.limit(50)
+  end
+
+  # GET: /users/:id
+  def show
+    @user = User.select(:name, :email).find_by(id: params[:id])
+  end
+
+  # GET: /users/new
+  def new
+  end
+
+  # GET: /users/:id/edit
+  def edit
+  end
+
+  # POST: /users
+  def create
+  end
+
+  # PUT: /users/:id
+  def update
+  end
+
+  # GET: /users/my_action/:email
+  def my_action
+    # params[:email]
+  end
+
+  # DELETE: /users/:id
+  def destroy
+  end
 end
--- project/config/routes.rb
+++ project/config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  root 'home#index'
+
+  # para filtrar actions, vc pode utilizar o except ou only
+  # ex:
+  # resources :users, only: [:index, :show]
+  # resources :users, except: [:destroy, :create, :update]
+  resources :users do
+    collection do
+      get 'my_action', action: :my_action
+    end
+  end
 
 end
